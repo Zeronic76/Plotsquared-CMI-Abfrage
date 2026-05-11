0d21b5ca20051e6ca99fc047e35985de08f57b23
Zeronic76 <35219454+Zeronic76@users.noreply.github.com>
2026-05-11 03:54:12 +0200

    Add English language file for localization

---
+++ lang/en.php
@@ -0,0 +1,15 @@
+<?php
+$lang = [
+    'title' => 'Plot-Admin',
+    'search_placeholder' => 'Name or UUID...',
+    'search_btn' => 'SEARCH',
+    'results_for' => 'Results for:',
+    'table_world' => 'WORLD',
+    'table_coords' => 'COORDINATES (X;Z)',
+    'table_owner' => 'OWNER',
+    'table_action' => 'ACTION',
+    'table_role' => 'ROLE',
+    'btn_visit' => 'COPY VISIT',
+    'no_plots' => 'No plots found.',
+    'error_mojang' => 'Player name could not be found at Mojang.'
+];
