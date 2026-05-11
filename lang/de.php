776b382808b0d73d73991b021c25631211bcc339
Zeronic76 <35219454+Zeronic76@users.noreply.github.com>
2026-05-11 03:53:43 +0200

    Add German language support in de.php

---
+++ lang/de.php
@@ -0,0 +1,15 @@
+<?php
+$lang = [
+    'title' => 'Plot-Admin',
+    'search_placeholder' => 'Name oder UUID...',
+    'search_btn' => 'SUCHEN',
+    'results_for' => 'Ergebnisse für:',
+    'table_world' => 'WELT',
+    'table_coords' => 'KOORDINATEN (X;Z)',
+    'table_owner' => 'BESITZER',
+    'table_action' => 'AKTION',
+    'table_role' => 'ROLLE',
+    'btn_visit' => 'VISIT KOPIEREN',
+    'no_plots' => 'Keine Grundstücke gefunden.',
+    'error_mojang' => 'Spielername konnte bei Mojang nicht gefunden werden.'
+];
