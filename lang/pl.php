f7b29795a5d27db9e36b00dec29150eab502d768
Zeronic76 <35219454+Zeronic76@users.noreply.github.com>
2026-05-11 03:54:56 +0200

    Add Polish language localization file

---
+++ lang/pl.php
@@ -0,0 +1,15 @@
+<?php
+$lang = [
+    'title' => 'Plot-Admin',
+    'search_placeholder' => 'Nazwa lub UUID...',
+    'search_btn' => 'SZUKAJ',
+    'results_for' => 'Wyniki dla:',
+    'table_world' => 'ŚWIAT',
+    'table_coords' => 'WSPÓŁRZĘDNE (X;Z)',
+    'table_owner' => 'WŁAŚCICIEL',
+    'table_action' => 'AKCJA',
+    'table_role' => 'ROLA',
+    'btn_visit' => 'KOPIUJ VISIT',
+    'no_plots' => 'Nie znaleziono działek.',
+    'error_mojang' => 'Nie znaleziono gracza w bazie Mojang.'
+];
