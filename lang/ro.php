81813bb8e1b5a2936b2b176ad4d992c6da476f7e
Zeronic76 <35219454+Zeronic76@users.noreply.github.com>
2026-05-11 03:54:35 +0200

    Add Romanian language file for localization

---
+++ lang/ro.php
@@ -0,0 +1,15 @@
+<?php
+$lang = [
+    'title' => 'Plot-Admin',
+    'search_placeholder' => 'Nume sau UUID...',
+    'search_btn' => 'CAUTĂ',
+    'results_for' => 'Rezultate pentru:',
+    'table_world' => 'LUME',
+    'table_coords' => 'COORDONATE (X;Z)',
+    'table_owner' => 'PROPRIETAR',
+    'table_action' => 'ACȚIUNE',
+    'table_role' => 'ROL',
+    'btn_visit' => 'COPIAZĂ VIZITA',
+    'no_plots' => 'Nu au fost găsite terenuri.',
+    'error_mojang' => 'Numele jucătorului nu a putut fi găsit la Mojang.'
+];
