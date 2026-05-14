241447f64dd962908e069729c6ee616548c9b492
Zeronic76 <35219454+Zeronic76@users.noreply.github.com>
2026-05-14 21:28:22 +0200

    Update database configuration in config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -12,37 +12,37 @@
  */
 return [
 
-    // Allgemeine Datenbank
-    'db_host' => '',
-    'db_port' => '3306',
-    'db_user' => '',
-    'db_pass' => '',
+    // Allgemeine DB-Daten
+    'db_host'   => 'localhost',
+    'db_port'   => '3306',
+    'db_user'   => 'root',
+    'db_pass'   => '',
 
     // PlotSquared
-    'db_plots' => 'plotsquared',         // Plotsquared Database
-    'table_plot' => 'plot',              // tablename for plot
-
-    // Sprache
-    'language' => 'de',
+    'db_plots'      => 'plotsquared',
+    'table_plot'    => 'plot',
+    'table_helpers' => 'plot_helpers',
+    'table_trusted' => 'plot_trusted',
 
     // Spielerquelle
-    'player_source' => 'cmi', // cmi | luckperms
+    'player_source' => 'cmi',
 
     // CMI
     'cmi' => [
-        'db'          => '',
-        'table_users' => 'CMI_users',
-        'col_uuid'    => 'uuid',
-        'col_name'    => 'username'
+        'db'           => 'citybuild',
+        'table_users'  => 'CMI_users',
+        'col_uuid'     => 'player_uuid',
+        'col_name'     => 'username'
     ],
 
     // LuckPerms
     'luckperms' => [
-        'db'          => '',
-        'table_users' => 'luckperms_players',
-        'col_uuid'    => 'uuid',
-        'col_name'    => 'username'
-    ]
+        'db'           => 'LuckPerms',
+        'table_users'  => 'luckperms_players',
+        'col_uuid'     => 'uuid',
+        'col_name'     => 'username'
+    ],
+
+    // Sprache
+    'language' => 'de',
 ];
-
-
