64aba9477158baf12de9a1acf4f9b4d0db89d9a0
Zeronic76 <35219454+Zeronic76@users.noreply.github.com>
2026-05-14 23:35:33 +0200

    Update French language translations in fr.php

--- lang/fr.php
+++ lang/fr.php
@@ -1,15 +1,15 @@
-<?php
+php
 $lang = [
-    'title' => 'Plot-Administrator',
-    'search_placeholder' => 'Navn eller UUID...',
-    'search_btn' => 'SØG',
-    'results_for' => 'Resultater for:',
-    'table_world' => 'VERDEN',
-    'table_coords' => 'KOORDINATER (X;Z)',
-    'table_owner' => 'EJER',
-    'table_action' => 'HANDLING',
-    'table_role' => 'ROLLE',
-    'btn_visit' => 'KOPIER BESØG',
-    'no_plots' => 'Ingen grunde fundet.',
-    'error_mojang' => 'Spillernavn kunne ikke findes hos Mojang.'
+    'title' => 'Admin des Parcelles',
+    'search_placeholder' => 'Nom ou UUID...',
+    'search_btn' => 'RECHERCHER',
+    'results_for' => 'Résultats pour :',
+    'table_world' => 'MONDE',
+    'table_coords' => 'COORDONNÉES (X;Z)',
+    'table_owner' => 'PROPRIÉTAIRE',
+    'table_action' => 'ACTION',
+    'table_role' => 'RÔLE',
+    'btn_visit' => 'COPIER VISITE',
+    'no_plots' => 'Aucune parcelle trouvée.',
+    'error_mojang' => 'Le nom du joueur n\'a pas pu être trouvé chez Mojang.'
 ];
