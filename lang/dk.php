80f6ea75a116355edcd8d6d5599a7588c0972de2
Zeronic76 <35219454+Zeronic76@users.noreply.github.com>
2026-05-14 23:34:40 +0200

    Add Danish language translations for the application

---
+++ lang/dk.php
@@ -0,0 +1,15 @@
+<?php
+$lang = [
+    'title' => 'Plot-Administrator',
+    'search_placeholder' => 'Navn eller UUID...',
+    'search_btn' => 'SØG',
+    'results_for' => 'Resultater for:',
+    'table_world' => 'VERDEN',
+    'table_coords' => 'KOORDINATER (X;Z)',
+    'table_owner' => 'EJER',
+    'table_action' => 'HANDLING',
+    'table_role' => 'ROLLE',
+    'btn_visit' => 'KOPIER BESØG',
+    'no_plots' => 'Ingen grunde fundet.',
+    'error_mojang' => 'Spillernavn kunne ikke findes hos Mojang.'
+];
