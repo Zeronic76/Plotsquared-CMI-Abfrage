8ddca00361c3664a6e504aba23980b819cd47eba
Zeronic76 <35219454+Zeronic76@users.noreply.github.com>
2026-05-14 23:32:49 +0200

    Add Spanish language file for localization

---
+++ lang/es.php
@@ -0,0 +1,15 @@
+<?php
+$lang = [
+    'title' => 'Administrador de Parcelas',
+    'search_placeholder' => 'Nombre o UUID...',
+    'search_btn' => 'BUSCAR',
+    'results_for' => 'Resultados para:',
+    'table_world' => 'MUNDO',
+    'table_coords' => 'COORDENADAS (X;Z)',
+    'table_owner' => 'PROPIETARIO',
+    'table_action' => 'ACCIÓN',
+    'table_role' => 'ROL',
+    'btn_visit' => 'COPIAR VISITA',
+    'no_plots' => 'No se encontraron parcelas.',
+    'error_mojang' => 'No se pudo encontrar el nombre de jugador en Mojang.'
+];
