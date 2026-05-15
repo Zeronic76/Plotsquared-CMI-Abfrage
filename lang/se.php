2e289aba633b4637addd519e7363dd8d1a77d75c
Zeronic76 <35219454+Zeronic76@users.noreply.github.com>
2026-05-15 02:06:41 +0200

    Add Swedish language support in se.php

---
+++ lang/se.php
@@ -0,0 +1,15 @@
+<?php
+$lang = [
+    'title' => 'Plot-Admin',
+    'search_placeholder' => 'Namn eller UUID...',
+    'search_btn' => 'SÖK',
+    'results_for' => 'Resultat för:',
+    'table_world' => 'VÄRLD',
+    'table_coords' => 'KOORDINATER (X;Z)',
+    'table_owner' => 'ÄGARE',
+    'table_action' => 'ÅTGÄRD',
+    'table_role' => 'ROLL',
+    'btn_visit' => 'KOPIERA BESÖK',
+    'no_plots' => 'Inga tomter hittades.',
+    'error_mojang' => 'Spelarnamn kunde inte hittas hos Mojang.'
+];
