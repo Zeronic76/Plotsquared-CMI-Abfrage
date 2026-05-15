9c34ae68bee3c9cd751fdb08be007d5bb1ee5a0c
Zeronic76 <35219454+Zeronic76@users.noreply.github.com>
2026-05-15 02:07:20 +0200

    Add Dutch language support for parcel management

---
+++ lang/nl.php
@@ -0,0 +1,15 @@
+<?php
+$lang = [
+    'title' => 'Perceelbeheer',
+    'search_placeholder' => 'Naam of UUID...',
+    'search_btn' => 'ZOEKEN',
+    'results_for' => 'Resultaten voor:',
+    'table_world' => 'WERELD',
+    'table_coords' => 'COÖRDINATEN (X;Z)',
+    'table_owner' => 'EIGENAAR',
+    'table_action' => 'ACTIE',
+    'table_role' => 'ROL',
+    'btn_visit' => 'KOPIEER BEZOEK',
+    'no_plots' => 'Geen percelen gevonden.',
+    'error_mojang' => 'Spelersnaam kon niet bij Mojang worden gevonden.'
+];
